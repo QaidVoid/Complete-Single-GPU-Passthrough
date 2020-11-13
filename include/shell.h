5c5ae77e2f2ab4942c12497f57907e25d9a6d45c
QaidVoid <12017109+QaidVoid@users.noreply.github.com>
2020-11-13 15:09:40 +0545

    Deleted C++ files

--- include/shell.h
+++
@@ -1,8 +0,0 @@
-#include <memory>
-#include <iostream>
-#include <algorithm>
-
-std::string shell_cmd(char const *cmd);
-void trim(std::string &s);
-void rtrim(std::string &s);
-void ltrim(std::string &s);
--- include/vm.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <shell.h>
-
-void create_vm();
-void create_vm_with_libvirt();
